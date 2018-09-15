016c043da533004199825d64f8beee835a5959ba
Yerin <yerin4847@naver.com>
2018-09-15 17:44:44 +0900

    w

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.utils import timezone
 from django.shortcuts import redirect
 
 
